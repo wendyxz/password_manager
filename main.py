aeecb066db8d7ca82aa478ed37cef7e800a150ed
Wendy Zhang <wendyyuxuanzhang@gmail.com>
2022-03-13 19:45:07 -0400

    upgrade data to json and add search function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- GENERATE PASSWORD ------------------------------- #
 def generate():
@@ -39,19 +40,50 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Details entered: \nemail: {email}"
-                                                      f"\npassword: {password} \nDo you want to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        #is_ok = messagebox.askokcancel(title=website, message=f"Details entered: \nemail: {email}"
+                                                      #f"\npassword: {password} \nDo you want to save?")
+        #if is_ok:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
 
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
             set_sliders()
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password= data[website]["password"]
+            messagebox.showinfo(title=website, message=f"email: {email}\npassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No password for {website} is stored")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -83,8 +115,8 @@ total_symbols_label = Label(text="Total symbols:")
 total_symbols_label.grid(row=7, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 email_entry = Entry(width=35)
@@ -95,11 +127,14 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate)
-generate_password_button.grid(row=3, column=2)
+generate_button = Button(text="Generate Password", command=generate)
+generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", width=15, command=search)
+search_button.grid(row=1, column=2)
 
 # Sliders
 letters_slider = Scale(window, from_=0, to=16, orient=HORIZONTAL)
